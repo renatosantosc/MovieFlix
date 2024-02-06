025e5197c954d43a35b205c1586148ee91b8f5e5
Renato Santos <renatosantosdeoliveirac@gmail.com>
2024-02-05 22:12:44 -0300

    Inserido o component Home no main

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
 import './index.css'
-import MyRoutes from './Routes/Routes'
+// import MyRoutes from './Routes/Routes'
+import Home from './Pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MyRoutes />
+    <Home />
   </React.StrictMode>,
 )
